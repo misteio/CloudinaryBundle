162e61f84f285b4c5fea58c4775cae8018e236a5
misteio <jonathan@headoo.com>
2015-10-16 22:52:33 +0200

    Create AppKernel.php

---
+++ Tests/App/AppKernel.php
@@ -0,0 +1,39 @@
+<?php
+
+use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Config\Loader\LoaderInterface;
+
+class AppKernel extends Kernel
+{
+    public function registerBundles()
+    {
+        return array(
+            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+            new Misteio\CloudinaryBundle\MisteioCloudinaryBundle(),
+            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
+            new Symfony\Bundle\TwigBundle\TwigBundle(),
+            new \Symfony\Bundle\AsseticBundle\AsseticBundle(),
+        );
+    }
+
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+        $loader->load(__DIR__.'/config/config_'.$this->getEnvironment().'.yml');
+    }
+
+    /**
+     * @return string
+     */
+    public function getCacheDir()
+    {
+        return sys_get_temp_dir().'/ChillMainBundle/cache';
+    }
+
+    /**
+     * @return string
+     */
+    public function getLogDir()
+    {
+        return sys_get_temp_dir().'/ChillMainBundle/logs';
+    }
+}
