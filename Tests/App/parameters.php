21b2580ab3b0da764926e527f5147ee5aa47c337
misteio <jonathan@headoo.com>
2015-10-17 10:23:41 +0200

    Update parameters.php

--- Tests/App/parameters.php
+++ Tests/App/parameters.php
@@ -1,4 +1,4 @@
 <?php
-    $container->setParameter('misteio_cloudinary.api_key', $_ENV['api_key']);
+    $container->setParameter('misteio_cloudinary.api_key1', $_ENV['api_key2']);
     $container->setParameter('misteio_cloudinary.secret_key', $_ENV['secret_key']);
     
